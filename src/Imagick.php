32d51662a917b6872f7053fc9381669618ce5bc8
Mong Dao <mongdaovan86@yahoo.com>
2020-08-25 13:52:03 +0700

    update 25082020

--- src/Imagick.php
+++ src/Imagick.php
@@ -292,6 +292,8 @@ class Imagick
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         $raw = curl_exec($ch);
         curl_close($ch);
         if (file_exists($img)) {
